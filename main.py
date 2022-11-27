aa67eba8e29f6a85d0340a33e4ceb2d7aa61a9cd
jakobeckerbom <61848436+jakobeckerbomdev@users.noreply.github.com>
2022-11-27 21:19:53 +0100

    error handling improvements

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 from rss_parser import Parser
 from requests import get
 import PySimpleGUI as sg
+import time
 import os
 import sys
 
 
 def main():
     # begin error handling
-    try:
-        sg.theme('DarkGrey')
-        # user inputs an url
-        user_url = sg.PopupGetText("Enter your URL: ")
-        # user inputs an integer for amount of entries
-        user_limit = int(sg.PopupGetText("How many entries would you like to see? "))
+    while True:
+        try:
+            sg.theme('DarkGrey')
+            # user inputs an url
+            user_url = sg.PopupGetText("Enter your URL: ")
+            # user inputs an integer for amount of entries
+            user_limit = int(sg.PopupGetText("How many entries would you like to see? "))
 
-        # translating user entries for rss-parser
-        rss_url = user_url
-        xml = get(rss_url)
-        # rss-parser logic
-        parser = Parser(xml=xml.content, limit=user_limit)
-        feed = parser.parse()
-    # small exception that handles both the url and the integer value
-    except ValueError:
-        sg.theme('DarkBrown4')
-        sg.popup("Enter valid values!")
+            # translating user entries for rss-parser
+            rss_url = user_url
+            xml = get(rss_url)
+            # rss-parser logic
+            parser = Parser(xml=xml.content, limit=user_limit)
+            feed = parser.parse()
+        # small exception that handles both the url and the integer value
+        except ValueError:
+            sg.theme('DarkBrown4')
+            sg.popup("Enter valid values!")
+            time.sleep(15)
+        else:
+            break
 
     def main_window():
         # window for reader output
